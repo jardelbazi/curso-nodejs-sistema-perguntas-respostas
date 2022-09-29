1ad9031f6320e305ada701e7c58c1370c016c3a1
Jardel Bazi <jardelbazi@gmail.com>
2022-09-29 16:48:55 -0300

    bootstrap + form + partials

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const express  = require("express")
+const bodyParser  = require("body-parser")
+
 const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-	let name = "Jardel Pavan Bazi"
-	let is_active = false
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-	let users = [
-		{name: "Jardel Bazi" ,age: 37},
-		{name: "Jaine Jover" ,age: 22},
-		{name: "Poliana" ,age: 35},
-	]
+app.get('/', (req, res) => res.render('index'))
+app.get('/ask', (req, res) => res.render('ask'))
 
-	res.render('index', {
-		name,
-		users,
-		is_active
-	})
+app.post('/ask', (req, res) => {
+	let title = req.body.title
+	let description = req.body.description
+	res.send(`Formulário Recebido: Titulo: ${title}`)
 })
 
 app.listen(port, () => console.log("APP rodando!"))
